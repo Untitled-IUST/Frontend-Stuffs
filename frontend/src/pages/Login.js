af74d0fe8c8bcfdbcbc7b7bb27a76fae973ab4ad
Ehsan <ehsan.ahmadpoor2002@gmail.com>
2023-03-27 10:08:39 +0430

    complete change

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,64 +1,103 @@
-import React , {useState} from "react";
-import { Link } from "react-router-dom";
+import { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from "../context/AuthProvider";
+import { Link } from 'react-router-dom';
+import axios from '../api/axios';
+const LOGIN_URL = '/customer/email';
 
-function Login(){
-  const validEmailAddress = new RegExp(
-    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-  );
-  const [emailAddress , setEmailAddress] = useState(null);
-  const [emailAddressError , setEmailAddressError] = useState(true);
-  const handleEmail = (event) => {
-    //check if email is valid and exists
-    if(validEmailAddress.test(event.taeget.value) === false){
-      setEmailAddressError("Invalid Email!");
+const Login = () => {
+    const { setAuth } = useContext(AuthContext);
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [email, password])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(LOGIN_URL,
+                JSON.stringify({ email, password }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            console.log(JSON.stringify(response?.data));
+            const accessToken = response?.data?.accessToken;
+            setAuth({ email, password,accessToken });
+            setEmail('');
+            setPassword('');
+            setSuccess(true);
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 400) {
+                setErrMsg('Missing Username or Password');
+            } else if (err.response?.status === 401) {
+                setErrMsg('Unauthorized');
+            } else {
+                setErrMsg('Login Failed');
+            }
+            errRef.current.focus();
+        }
     }
-    //check email exists if it does put email error to false (if it doesnt put error to doesnt exist) and set email address to value
-  }
-  const handlePassword = (event) => {
-    //check if password is correct
-  }
-  const handleLogin = (event) => {
 
-  }
-  return(
-    <div className="LoginPage">
-      <div className="form">
-        <div className="LoginBanner">
-          <h1>Login</h1>
-        </div>
-        <form className="LoginForm">
-          <div className="emailAddress">
-            <div className="emailAddress-label">
-              <label className="label">email:</label>
-            </div>
-            <div className="emailAddress-input">
-              <input type="email" placeholder="Enter your email" onChange={handleEmail}/>
-            </div>
-          </div>
-          <div className="password">
-            <div className="password-label">
-              <label className="label">password</label>
-            </div>
-            <div className="password-input">
-              <input type="password" placeholder="Enter your password"/>
-            </div>
-          </div>
-          <div className="SubmitButton">
-            <div className="submit-button">
-              <button type="submit" className="submitButton" onClick={handleLogin}>
-                Login
-              </button>
-            </div>
-          </div>
-        </form>
-        <div>
-          <Link to="/SignUp">
-            dont have an account?SignUp
-          </Link>
-        </div>
-      </div>
-    </div>
-  )
+    return (
+        <>
+            {success ? (
+                <section>
+                    <h1>You are logged in!</h1>
+                    <br />
+                    <p>
+                        <a href="/">Go back</a>
+                    </p>
+                </section>
+            ) : (
+                <section>
+                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                    <h1>Login</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                            required
+                        />
+
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            required
+                        />
+                        <button>Sign In</button>
+                    </form>
+                    <p>
+                        <Link to="/SignUp">
+                          Need an account?SignUp
+                        </Link>
+                    </p>
+                </section>
+            )}
+        </>
+    )
 }
 
-export default Login;
+export default Login
