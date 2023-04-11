8e62d17d0d60cc26b43e4edcfdfa5b41740fccc5
Ehsan <ehsan.ahmadpoor2002@gmail.com>
2023-04-11 22:19:03 +0430

    salon page cards done.

--- frontend/src/components/salonCard.js
+++ frontend/src/components/salonCard.js
@@ -1,8 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { Link }from "react-router-dom";
-import backGroundImage from "../images/background.jpg";
-import barberProfile from "../images/woman_barber_profile.jpg"
 
 function SalonCard(information){
   
@@ -13,21 +11,24 @@ function SalonCard(information){
   const [image1 , setImage1] = useState(null);
   const [image2 , setImage2] = useState(null);
 
-  setBarberShop(information.name);
-  setAddress(information.address);
-  setPhoneNumber(information.phoneNumber);
-  setRate(information.rate);
-  setImage1(information.image1);
-  setImage2(information.image2);
+  useEffect( () => {
+    setBarberShop(information.name);
+    setAddress(information.address);
+    setPhoneNumber(information.phoneNumber);
+    setRate(information.rate);
+    setImage1(information.image1);
+    setImage2(information.image2);
+  },[])
+
 
   return(
-    <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <Link className="relative" to="/">
-            <img className="w-100 rounded-t-lg" src={image1} alt="salon page" />
+            <img className="w-full h-60 rounded-t-lg" src={image1} alt="salon page" />
             <div className="justify-items-center -mt-11 grid grid-cols-3">
               <div></div>
               <div>
-                <img className=" w-24 ring-4 ring-blue-300 rounded-full" src={image2} alt="profile" />
+                <img className="h-24 w-24 ring-4 ring-blue-300 rounded-full" src={image2} alt="profile" />
               </div>
               <div></div>
             </div>
@@ -38,7 +39,7 @@ function SalonCard(information){
             </Link>
             <p className="text-left mb-3 font-normal text-gray-700 dark:text-gray-400">{address}</p>
             <p className="text-left mb-3 font-normal text-gray-700 dark:text-gray-400">{phoneNumber}</p>
-            <div class="flex items-center">
+            <div class="flex mb-2 items-center">
               <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
               <p class="ml-2 text-sm font-bold text-gray-900 dark:text-white">{rate}</p>
             </div>
--- frontend/src/pages/salonSelect/salonSelect.jsx
+++ frontend/src/pages/salonSelect/salonSelect.jsx
@@ -1,29 +1,33 @@
 import React , { useState , useEffect} from "react";
 import axios from "axios";
 import SalonCard from "../../components/salonCard";
-
 const SalonSelect = () => {
+  
   const [data , setData] = useState("");
   const [error, setError] = useState(null);
-
+  
   useEffect( () => {
-    axios.get("https://amirmohammadkomijani.pythonanywhere.com/barber/info/")
-         .then(res => setData(res.data))
-         .catch(error => setError(error.response.data));
-  },[]);
-  console.log(data);
+    axios
+      .get("https://amirmohammadkomijani.pythonanywhere.com/barber/info/")
+      .then((res) => {
+        setData(res.data)
+      })
+      .catch(error => setError(error.response.data));
+    },[]);
+    
 
-  return(
-    <div>
-      <ul>
-          <li>
-                       
-           <SalonCard name={data["BarberShop"]} phoneNumber={data["phone_Number"]} address={data["address"]} rate={data["rate"]} image1={data["images"][0]} image2={data["images"][1]}/> 
-          </li>
-      </ul>
-    </div>
-
-  )
-}
-
+    return(
+      <div>
+        <div>
+          
+        </div>
+        <div className="bg-black grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 md:grid-cols-3 gap-4">
+          {Object.keys(data).map(id =>(
+            <div key={id}>
+            <SalonCard name={data[id]["BarberShop"]} phoneNumber={data[id]["phone_Number"]} address={data[id]["address"]} rate={data[id]["rate"]} image1={data[id]["images"][0]["background"]} image2={data[id]["images"][0]["logo"]}/> 
+            </div>
+          ))}
+        </div>
+      </div>
+  )}
 export default SalonSelect;
