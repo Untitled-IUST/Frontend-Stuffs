a3206c3ca57c4e4ade3ee02c77dcf4a2eec9c812
Ehsan <ehsan.ahmadpoor2002@gmail.com>
2023-04-12 09:50:55 +0430

    paging functionality done

--- frontend/src/components/salonCard.js
+++ frontend/src/components/salonCard.js
@@ -2,28 +2,29 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { Link }from "react-router-dom";
 
-function SalonCard(information){
+function SalonCard({id,name,address,phoneNumber,rate,image1,image2}){
   
-  const [BarberShop , setBarberShop] = useState(null);
-  const [address , setAddress] = useState(null);
-  const [phoneNumber , setPhoneNumber] = useState(null);
-  const [rate , setRate] = useState(null);
-  const [image1 , setImage1] = useState(null);
-  const [image2 , setImage2] = useState(null);
+  // const [BarberShop , setBarberShop] = useState(null);
+  // const [address , setAddress] = useState(null);
+  // const [phoneNumber , setPhoneNumber] = useState(null);
+  // const [rate , setRate] = useState(null);
+  // const [image1 , setImage1] = useState(null);
+  // const [image2 , setImage2] = useState(null);
 
-  useEffect( () => {
-    setBarberShop(information.name);
-    setAddress(information.address);
-    setPhoneNumber(information.phoneNumber);
-    setRate(information.rate);
-    setImage1(information.image1);
-    setImage2(information.image2);
-  },[])
+  // useEffect( () => {
+  //   setBarberShop(information.name);
+  //   setAddress(information.address);
+  //   setPhoneNumber(information.phoneNumber);
+  //   setRate(information.rate);
+  //   setImage1(information.image1);
+  //   setImage2(information.image2);
+  // },[])
+  const path =  "https://amirmohammadkomijani.pythonanywhere.com/barber/info/"+id+"/";
 
 
   return(
     <div className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <Link className="relative" to="/">
+        <Link className="relative" to={path}>
             <img className="w-full h-60 rounded-t-lg" src={image1} alt="salon page" />
             <div className="justify-items-center -mt-11 grid grid-cols-3">
               <div></div>
@@ -34,14 +35,14 @@ function SalonCard(information){
             </div>
         </Link>
         <div className="p-5">
-            <Link to="/">
-                <h5 className="text-left mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{BarberShop}</h5>
+            <Link to={path}>
+                <h5 className="text-left mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
             </Link>
             <p className="text-left mb-3 font-normal text-gray-700 dark:text-gray-400">{address}</p>
             <p className="text-left mb-3 font-normal text-gray-700 dark:text-gray-400">{phoneNumber}</p>
-            <div class="flex mb-2 items-center">
-              <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-              <p class="ml-2 text-sm font-bold text-gray-900 dark:text-white">{rate}</p>
+            <div className="flex mb-2 items-center">
+              <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+              <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">{rate}</p>
             </div>
               <Link to="/" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                   Visit salon page
--- frontend/src/pages/salonSelect/salonSelect.jsx
+++ frontend/src/pages/salonSelect/salonSelect.jsx
@@ -1,21 +1,47 @@
 import React , { useState , useEffect} from "react";
 import axios from "axios";
 import SalonCard from "../../components/salonCard";
+import { Pagination, Typography } from "@mui/material";
+
 const SalonSelect = () => {
   
   const [data , setData] = useState("");
   const [error, setError] = useState(null);
+  const [page ,setPage] = useState(1);
+  const baseURL = "https://amirmohammadkomijani.pythonanywhere.com/barber/info/"
+  const [continueApi , setContinueApi] = useState("");
+  const [howManyPages, setHowManyPages] = useState(Math.ceil(data["count"]/9));
+
+  const handleChange = (event,value) => {
+    setPage(value);
+  }
+
+  useEffect(() => {
+    if(page === 1){
+      setContinueApi("");
+    }
+    else{
+      setContinueApi("?page="+page);
+    }
+  }, [page]);
+ 
+  useEffect ( () => {
+    const fetchData = async () => {
+      const result = await axios(
+        baseURL+continueApi
+      );
+      setData(result.data["results"]);
+      setHowManyPages(Math.ceil(result.data["count"]/9));
+    };
+    fetchData();
+  },[continueApi])
   
   useEffect( () => {
-    axios
-      .get("https://amirmohammadkomijani.pythonanywhere.com/barber/info/")
-      .then((res) => {
-        setData(res.data)
-      })
-      .catch(error => setError(error.response.data));
-    },[]);
+    console.log(page);
+    console.log(continueApi);
+    console.log(data);
+  },[data])
     
-
     return(
       <div>
         <div>
@@ -24,9 +50,12 @@ const SalonSelect = () => {
         <div className="bg-black grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 md:grid-cols-3 gap-4">
           {Object.keys(data).map(id =>(
             <div key={id}>
-            <SalonCard name={data[id]["BarberShop"]} phoneNumber={data[id]["phone_Number"]} address={data[id]["address"]} rate={data[id]["rate"]} image1={data[id]["images"][0]["background"]} image2={data[id]["images"][0]["logo"]}/> 
+            <SalonCard id={data[id]["id"]} name={data[id]["BarberShop"]} phoneNumber={data[id]["phone_Number"]} address={data[id]["address"]} rate={data[id]["rate"]} image1={data[id]["images"][0]["background"]} image2={data[id]["images"][0]["logo"]}/> 
             </div>
           ))}
+        </div>
+        <div className="p-2 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 bg-pink-400	" style={{position: "fixed", bottom: "77px"}}>
+          <Pagination count={howManyPages} page={page} color="secondary" onChange={handleChange}/>
         </div>
       </div>
   )}
