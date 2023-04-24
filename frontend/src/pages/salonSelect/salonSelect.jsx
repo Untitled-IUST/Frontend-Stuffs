86644e973ff12fa30e3cceca1f4632f10aa163ab
Ehsan <ehsan.ahmadpoor2002@gmail.com>
2023-04-24 22:34:38 +0430

    bruh moment with the indexing of data mapping

--- frontend/src/pages/salonSelect/salonSelect.jsx
+++ frontend/src/pages/salonSelect/salonSelect.jsx
@@ -1,61 +1,52 @@
 import React , { useState , useEffect} from "react";
 import axios from "axios";
 import SalonCard from "../../components/salonCard";
-import { Pagination, Typography } from "@mui/material";
+import { Pagination } from "@mui/material";
 
 const SalonSelect = () => {
   
-  const [data , setData] = useState("");
-  const [page ,setPage] = useState(1);
-  const baseURL = "https://amirmohammadkomijani.pythonanywhere.com/barber/info/"
+  const [data , setData] = useState([]);
   const [continueApi , setContinueApi] = useState("");
-  const [howManyPages, setHowManyPages] = useState(Math.ceil(data["count"]/9));
+  const [page ,setPage] = useState(1);
+  const [howManyPages, setHowManyPages] = useState(0);
+  const baseURL = "https://amirmohammadkomijani.pythonanywhere.com/barber/info/"
 
+  useEffect(() => {
+    async function fetchData(){
+      try{
+        const response = await fetch(baseURL+continueApi);
+        const fetchedData = await response.json();
+        setData(fetchedData.results);
+        setHowManyPages(Math.ceil(fetchedData.count/9));
+      } catch(error){
+        console.log(error);
+      }
+    }
+    fetchData();
+  },[continueApi])
+
+  
   const handleChange = (event,value) => {
     setPage(value);
   }
 
-  useEffect(() => {
-    if(page === 1){
-      setContinueApi("");
-    }
-    else{
-      setContinueApi("?page="+page);
-    }
-  }, [page]);
- 
-  useEffect ( () => {
-    const fetchData = async () => {
-      const result = await axios(
-        baseURL+continueApi
-      );
-      setData(result.data.results);
-      setHowManyPages(Math.ceil(result.data["count"]/9));
-    };
-    fetchData();
-  },[continueApi])
-  
-  useEffect( () => {
-    console.log(page);
-    console.log(continueApi);
-    console.log(data);
-  },[data])
-    
     return(
       <div>
-        <div>
-          
+        <div className="justify-center max-w-[1400px] flex flex-col sm:flex-row-reverse">
+          <aside className="m-4  bg-pink-400 ">
+            ff
+          </aside>
+          <div className="flex-1 m-4 bg-black grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-1">
+            {data.map((item) =>(
+              <div key={item.id}>
+              <SalonCard id={item.id} name={item.BarberShop} phoneNumber={item.phone_Number} address={item.address} rate={item.rate} background={item.background} logo={item.logo}/> 
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="bg-black grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 md:grid-cols-3 gap-4">
-          {Object.keys(data).map(id =>(
-            <div key={id}>
-            <SalonCard id={data[id]["id"]} name={data[id]["BarberShop"]} phoneNumber={data[id]["phone_Number"]} address={data[id]["address"]} rate={data[id]["rate"]} image1={data[id]["images"][0]["background"]} image2={data[id]["images"][0]["logo"]}/> 
-            </div>
-          ))}
-        </div>
-        <div className="p-2 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 bg-pink-400	" style={{position: "fixed", bottom: "77px"}}>
+        {/* <div className="p-2 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 bg-pink-400	" style={{position: "fixed", bottom: "77px"}}>
           <Pagination count={howManyPages} page={page} color="secondary" onChange={handleChange}/>
-        </div>
+        </div> */}
       </div>
   )}
 export default SalonSelect;
