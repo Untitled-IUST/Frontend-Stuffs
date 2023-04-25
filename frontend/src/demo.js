e9b5988d616bc64af34c288c17f3262cb4213fb7
fargol <ffereydounifargol@gmail.com>
2023-04-25 17:31:45 +0430

    sidebar functionality fixed

--- frontend/src/demo.js
+++ frontend/src/demo.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Switch , Route, Link, Routes, BrowserRouter } from 'react-router-dom';
 import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import MuiDrawer from '@mui/material/Drawer';
@@ -18,6 +19,11 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import HomeIcon from '@mui/icons-material/Home';
+import ContentCutIcon from '@mui/icons-material/ContentCut';
+import PersonIcon from '@mui/icons-material/Person';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import { BlankPage } from "./Components/Blank";
 
 const drawerWidth = 240;
 
@@ -78,10 +84,19 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
     ...(open && {
       ...openedMixin(theme),
       '& .MuiDrawer-paper': openedMixin(theme),
+      '& .MuiDrawer-paper': {
+        backgroundColor: '#261b39',
+        color: '#fecbca',
+        ...openedMixin(theme),
+      },
     }),
     ...(!open && {
       ...closedMixin(theme),
       '& .MuiDrawer-paper': closedMixin(theme),
+      '& .MuiDrawer-paper': {
+        backgroundColor: 'black',
+        ...closedMixin(theme),
+      },
     }),
   }),
 );
@@ -98,23 +113,17 @@ export default function MiniDrawer() {
     setOpen(false);
   };
 
+
+
   return (
+    <Router>
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
         <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{
-              marginRight: 5,
-              ...(open && { display: 'none' }),
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
+          <IconButton color="inherit" aria-label="open drawer" onClick={handleDrawerOpen} edge="start" sx={{ marginRight: 5, ...(open && { display: 'none' }), }} >
+            <MenuIcon sx={{ color: '#fecbca' }} />
+          </IconButton >
           <Typography variant="h6" noWrap component="div">
             Mini variant drawer
           </Typography>
@@ -127,48 +136,64 @@ export default function MiniDrawer() {
           </IconButton>
         </DrawerHeader>
         <Divider />
-        <List>
-          {['Home', 'Dashboard', 'User', 'Hairdresser'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                  }}
-                >
+        {/* <List>
+          {[{ text: 'Home', path: '/' },
+            { text: 'Dashboard', path: '/dashboard' },
+            { text: 'User', path: '/user' },
+            { text: 'Hairdresser', path: '/hairdresser' },
+          ].map((item, index) => (
+
+            <ListItem key={item.text} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton sx={{ minHeight: 48, justifyContent: open ? 'initial' : 'center', px: 2.5, color: '#fecbca' }} >
+                <ListItemIcon sx={{ minWidth: 0, mr: open ? 3 : 'auto', justifyContent: 'center', color: '#fecbca' }} >
                   {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                 </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                <ListItemText primary={item.text} sx={{ opacity: open ? 1 : 0 }} />
               </ListItemButton>
             </ListItem>
           ))}
+          
+        </List> */}
+        <List>
+          <Link to="/" style={{ textDecoration: 'none', color: '#fecbca' }}>
+            <ListItemButton sx={{ minHeight: 48, justifyContent: open ? 'initial' : 'center', px: 2.5 }} >
+              <ListItemIcon sx={{ minWidth: 0, mr: open ? 3 : 'auto', justifyContent: 'center', color: '#fecbca' }} >
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary='Home' sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </Link>
+          <Link to="/dashboard" style={{ textDecoration: 'none', color: '#fecbca' }}>
+            <ListItemButton sx={{ minHeight: 48, justifyContent: open ? 'initial' : 'center', px: 2.5 }} >
+              <ListItemIcon sx={{ minWidth: 0, mr: open ? 3 : 'auto', justifyContent: 'center', color: '#fecbca' }} >
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary='Dashboard' sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </Link>
+          <Link to="/user" style={{ textDecoration: 'none', color: '#fecbca' }}>
+            <ListItemButton sx={{ minHeight: 48, justifyContent: open ? 'initial' : 'center', px: 2.5 }} >
+              <ListItemIcon sx={{ minWidth: 0, mr: open ? 3 : 'auto', justifyContent: 'center' , color: '#fecbca'}} >
+                <PersonIcon />
+              </ListItemIcon>
+              <ListItemText primary='User' sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </Link>
+          <Link to="/hairdresser" style={{ textDecoration: 'none', color: '#fecbca' }}>
+            <ListItemButton sx={{ minHeight: 48, justifyContent: open ? 'initial' : 'center', px: 2.5 }} >
+              <ListItemIcon sx={{ minWidth: 0, mr: open ? 3 : 'auto', justifyContent: 'center', color: '#fecbca' }} >
+                <ContentCutIcon />
+              </ListItemIcon>
+              <ListItemText primary='Hairdresser' sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </Link>
         </List>
         <Divider />
         <List>
           {['Log out'].map((text, index) => (
             <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                  }}
-                >
+              <ListItemButton sx={{ minHeight: 48, justifyContent: open ? 'initial' : 'center', px: 2.5, color: '#fecbca' }} >
+                <ListItemIcon sx={{  minWidth: 0, mr: open ? 3 : 'auto', justifyContent: 'center', color: '#fecbca' }} >
                   {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                 </ListItemIcon>
                 <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
@@ -177,36 +202,24 @@ export default function MiniDrawer() {
           ))}
         </List>
       </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Box>
+
+
+    
+            {/* <Routes>
+                <Route path="/">
+                    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                    <DrawerHeader />
+                    <Typography paragraph>
+                        Hello !!!
+                    </Typography>
+                    </Box>
+                </Route>
+                <Route path="/user">2</Route>
+            </Routes> */}
+
+      
     </Box>
+    </Router>
+    
   );
 }
