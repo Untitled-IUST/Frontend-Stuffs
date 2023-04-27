fcee9b3ab48bf1f71d6c7d8951389cd6fb47145a
Ehsan <ehsan.ahmadpoor2002@gmail.com>
2023-04-27 15:40:24 +0430

    rate finished

--- frontend/src/pages/salonSelect/salonSelect.jsx
+++ frontend/src/pages/salonSelect/salonSelect.jsx
@@ -7,54 +7,82 @@ import Button from "@mui/material/Button";
 const SalonSelect = () => {
   
   const [data , setData] = useState([]);
-  const [continueApi , setContinueApi] = useState("");
-  const [page ,setPage] = useState(1);
   const [howManyPages, setHowManyPages] = useState(0);
   const baseURL = "https://amirmohammadkomijani.pythonanywhere.com/barber/info/"
 
   useEffect(() => {
     async function fetchData(){
-      try{
-        const response = await fetch(baseURL+continueApi);
-        const fetchedData = await response.json();
-        setData(fetchedData.results);
-        setHowManyPages(Math.ceil(fetchedData.count/9));
-      } catch(error){
-        console.log(error);
-      }
+      
+      const response = await fetch(baseURL);
+      const fetchedData = await response.json();
+      setData(fetchedData.results);
+      setHowManyPages(Math.ceil(fetchedData.count/9));
+      response = await fetch()
     }
     fetchData();
-  },[continueApi])
+  },[])
 
   
   const handleChange = (event,value) => {
-    setPage(value);
+    
   }
 
-  const ContinueURLForFiltersAddress = "?address=";
-  const ContinueURLForFiltersRate = "&rate__gt=";
-
+  const ContinueURLForFiltersTopRate = "&rate__lte=";
+  const ContinueURLForFiltersArea = "?area=";
+  const ContinueURLForFiltersLowRate = "&rate__gte=";
+  const ContinueURLForFiltersPage = "&page=";
+  
   const handleApplyFilters = (event) => {
+
     async function fetchData(){
-      try{
-        const response = await fetch(baseURL+ContinueURLForFiltersAddress+addressValue+ContinueURLForFiltersRate+rateValue);
-        const fetchedData = await response.json();
-        setData(fetchedData.results);
-        setHowManyPages(Math.ceil(fetchedData.count/9));
-      } catch(error){
-        console.log(error);
+      if(page == 1){
+        try{
+          const response = await fetch(baseURL+ContinueURLForFiltersArea+area+ContinueURLForFiltersLowRate+rateValue[0]+ContinueURLForFiltersTopRate+rateValue[1]);
+          const fetchedData = await response.json();
+          setData(fetchedData.results);
+          setHowManyPages(Math.ceil(fetchedData.count/9));
+        } catch(error){
+          console.log(error);
+        }
+      }
+      else{
+        try{
+          const response = await fetch(baseURL+ContinueURLForFiltersArea+area+ContinueURLForFiltersPage+page+ContinueURLForFiltersTopRate+rateValue[0]+ContinueURLForFiltersLowRate+rateValue[1]);
+          const fetchedData = await response.json();
+          setData(fetchedData.results);
+          setHowManyPages(Math.ceil(fetchedData.count/9));
+        } catch(error){
+          console.log(error);
+        }
       }
     }
     fetchData();
   }
 
+
   const [rateDropdown , setRateDropdown] = useState(false);
-  const [rateValue , setRateValue] = useState("");
-  const [addressValue,setAddressValue] = useState("");
+  const [areaDropdown , setAreaDropdown] = useState(false);
+  const [area , setArea] = useState("");
+  const [page , setPage] = useState(1);
+  const [rateValue , setRateValue] = useState([0,5]);
+
+  function valueText(value) {
+    return `${value}`;
+  }
 
     return(
       <div>
         <div className="mx-auto w-full max-w-[1400px] flex flex-col sm:flex-row-reverse">
+          {/* <div className="w-fit h-fit relative">
+            <button className="w-[50px] h-[50px] border-none text-base font-thin outline-none cursor-pointer border rounded-full absolute right-0 bg-black focus:">
+              <i>
+
+              </i>
+            </button>
+            <input>
+
+            </input>
+          </div> */}
           <aside className="flex-1 m-4">
             <div className="m-2 p-4 flex flex-col items-start justify-start bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <button onClick={() => {setRateDropdown(!rateDropdown)}} id="dropdowmButton" type="button" className="m-1 p-1 w-full flex justify-between" >
@@ -69,8 +97,12 @@ const SalonSelect = () => {
                 <div className="ml-2 flex flex-col">
                   <hr className="mb-2" />
                   <div className="m-0.5 flex flex-row-5">
-                    <input id="0" type="radio" name="rateMin" value={0} checked={rateValue==0} onChange={(event) => {setRateValue(event.target.value)}}/>
-                    <label for="0" className="flex flex-row">
+                    <input id="0to1" type="radio" name="RateMinMax" value={[0,1]} checked={(rateValue[0]>=0 && rateValue[1]<=1)} 
+                      onChange={(event) => {
+                        let x = [event.target.value[0] , event.target.value[2]];
+                        setRateValue(x);
+                      }}/>
+                    <label for="0to1" className="flex flex-row">
                       <svg aria-hidden="true" className="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                       <svg aria-hidden="true" className="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                       <svg aria-hidden="true" className="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -79,8 +111,12 @@ const SalonSelect = () => {
                     </label>
                   </div>
                   <div className="m-0.5 flex flex-row-5">
-                    <input id="1" type="radio" name="rateMin" value={1} checked={rateValue==1} onChange={(event) => {setRateValue(event.target.value)}}/>
-                    <label for="1" className="flex flex-row">
+                    <input id="1to2" type="radio" name="RateMinMax" value={[1,2]} checked={(rateValue[0]>=1 && rateValue[1]<=2)} 
+                      onChange={(event) => {
+                        let x = [event.target.value[0] , event.target.value[2]];
+                        setRateValue(x);
+                      }}/>
+                    <label for="1to2" className="flex flex-row">
                       <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                       <svg aria-hidden="true" className="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                       <svg aria-hidden="true" className="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -89,8 +125,12 @@ const SalonSelect = () => {
                     </label>
                   </div>
                   <div className="m-0.5 flex flex-row-5">
-                    <input id="2" type="radio" name="rateMin" value={2} checked={rateValue==2} onChange={(event) => {setRateValue(event.target.value)}}/>
-                    <label for="2" className="flex flex-row">
+                    <input id="2to3" type="radio" name="RateMinMax" value={[2,3]} checked={(rateValue[0]>=2 && rateValue[1]<=3)} 
+                      onChange={(event) => {
+                        let x = [event.target.value[0] , event.target.value[2]];
+                        setRateValue(x);
+                      }}/>
+                    <label for="2to3" className="flex flex-row">
                       <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                       <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                       <svg aria-hidden="true" className="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -99,8 +139,12 @@ const SalonSelect = () => {
                     </label>
                   </div>
                   <div className="m-0.5 flex flex-row-5">
-                    <input id="3" type="radio" name="rateMin" value={3} checked={rateValue===3} onChange={(event) => {setRateValue(event.target.value)}}/>
-                    <label for="3" className="flex flex-row">
+                    <input id="3to4" type="radio" name="RateMinMax" value={[3,4]} checked={(rateValue[0]>=3 && rateValue[1]<=4)} 
+                      onChange={(event) => {
+                        let x = [event.target.value[0] , event.target.value[2]];
+                        setRateValue(x);
+                      }}/>
+                    <label for="3to4" className="flex flex-row">
                       <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                       <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                       <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -109,8 +153,12 @@ const SalonSelect = () => {
                     </label>
                   </div>
                   <div className="m-0.5 flex flex-row-5">
-                    <input id="4" type="radio" name="rateMin" value={4} checked={rateValue===4} onChange={(event) => {setRateValue(event.target.value)}}/>
-                    <label for="4" className="flex flex-row">
+                    <input id="4to5" type="radio" name="RateMinMax" value={[4,5]} checked={(rateValue[0]>=4 && rateValue[1]<=5)} 
+                      onChange={(event) => {
+                        let x = [event.target.value[0] , event.target.value[2]];
+                        setRateValue(x);
+                      }}/>
+                    <label for="4to5" className="flex flex-row">
                       <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                       <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                       <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Rating star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -119,13 +167,26 @@ const SalonSelect = () => {
                     </label>
                   </div>
                   <div className="ml-2 ">
-                    <Slider size="small" step={0.1}  min={0} max={5} valueLabelDisplay="auto" aria-label="rate" value={rateValue} onChange={(event) => {setRateValue(event.target.value)}}/>
-                  </div>
-                  <div>
-                    <Button size="small" variant="contained" onClick={handleApplyFilters}>Apply</Button>
+                    <Slider size="small" step={0.1}  min={0} max={5} valueLabelDisplay="auto" aria-label="rate" value={rateValue} getAriaValueText={valueText} onChange={(event) => {setRateValue(event.target.value)}}/>
                   </div>
                 </div>
               }
+              <button onClick={() => {setAreaDropdown(!rateDropdown)}} id="dropdowmButton" type="button" className="m-1 p-1 w-full flex justify-between" >
+                Area
+                <svg className="w-4 h-4 ml-2 mt-1" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                </svg>
+              </button>
+              {
+                areaDropdown && 
+                <div>
+                  
+                </div>
+              }
+              <div>
+                <Button size="small" variant="contained" onClick={handleApplyFilters} >Apply</Button>
+              </div>
             </div>
           </aside>
           <div className="sm:w-3/5 m-4 lg:w-3/4 bg-black grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-1">
