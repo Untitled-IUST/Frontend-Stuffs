386e149ceb2b4af3aadfcce5006e819a7f221022
maryam behroozi <m_behroozi@comp.iust.ac.ir>
2023-04-30 21:54:19 +0330

    wallet css

--- frontend/src/pages/DenominationItem.jsx
+++ frontend/src/pages/DenominationItem.jsx
@@ -1,12 +1,12 @@
 import '../css/DenominationItem.css'
 const DenominationItem = props => {
-  const {value, stateChange} = props
+  const {value, stateChange, onButtonClick} = props
   const changeState = () => {
     stateChange(value)
   }
   return (
     <li className="item-holder">
-      <button type="button" className="btn" onClick={changeState}>
+      <button type="button" className="btn" onClick={() => onButtonClick(value)}>
         {value}
       </button>
     </li>
--- frontend/src/pages/Wallet.jsx
+++ frontend/src/pages/Wallet.jsx
@@ -2,22 +2,27 @@ import { useState } from 'react';
 import '../css/Wallet.css';
 import DenominationItem from './DenominationItem';
 import { dList } from './DList';
+import { useNavigate } from 'react-router-dom';
 
 function CashWithdrawal (){
   const [money, setMoney] = useState(2000);
+  const navigate = useNavigate();
 
   const stateChange = (value) => {
     setMoney((prevState) => prevState + value);
   };
+
+  const handleButtonClick = (value) => {
+    navigate('/payment', { state: { value: value, stateChange: stateChange } });
+  }
+
 
   // const { dList}=props;
   return (
     <div className="main-container">
       <div className="inner-container">
         <div className="heading-container">
-          <div className="para-div">
-            <p className="s">S</p>
-          </div>
+           <img src={"https://s2.uupload.ir/files/348ad8c26d7ff7b6c23fe3e30f3e44dd_ducd.jpg"} alt='user image' className='pmr' />
           <p className="name-para">Sarah Williams</p>
         </div>
         <div className="money-container">
@@ -26,9 +31,8 @@ function CashWithdrawal (){
             <p className="amount">{money}</p>
           </div>
         </div>
-        <p className="rupees">In Rupees</p>
-        <p className="withdraw">Withdraw</p>
-        <p className="choose">CHOOSE SUM (IN RUPEES)</p>
+        <p className="withdraw">Add Money</p>
+        <p className="choose">CHOOSE SUM</p>
 
         <ul className="items-holder">
         {dList.map(
@@ -37,6 +41,7 @@ function CashWithdrawal (){
               key={eachObject.id}
               value={eachObject.value}
               stateChange={stateChange}
+              onButtonClick={handleButtonClick}
             />
           ))}
 
