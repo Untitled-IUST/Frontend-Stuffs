31a5ef000fb2a178726438ee22e2fd802d3273ee
Ehsan <ehsan.ahmadpoor2002@gmail.com>
2023-05-11 18:06:49 +0330

    SignUp barber design change

--- frontend/src/pages/LoginSignup/SignUpBarber.jsx
+++ frontend/src/pages/LoginSignup/SignUpBarber.jsx
@@ -141,173 +141,191 @@ function SignUpBarber(){
   }
 
   return(
-    <div className="container mx-auto">
-      <div className="flex justify-center px-6 my-12">
-        <div className="w-full xl:w-3/4 lg:w-11/12 flex border rounded shadow appearance-none">
-          <div className="w-full h-auto bg-white hidden lg:block lg:w-5/12 bg-cover rounded-l-lg">
-            <img className="mt-10" src={backGroundImageBarberSignUp} alt="SignUp" />
-          </div>
-          <div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
-            <h3 className="pt-4 text-2xl text-center">Sign Up Your Salon</h3>
-            <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
-            <div className="mb-4">
-								<label className="block mb-2 text-sm font-bold text-gray-700" for="Username">
-									Username
-								</label>
-								<input
-									className="focus:placeholder-gray-500 focus:border-gray-600 w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-									id="Username"
-									type="text"
-									placeholder="Username"
-                  onChange={handleUsername}
-								/>
-                <p className="text-xs italic text-red-500">{usernameError}</p>
-							</div>
+    <div className="bg-charcoal-500 flex items-center h-screen">
+      <div className="container mx-auto">
+        <div className="flex justify-center mx-3">
+          <div className="bg-charcoal-500 w-full flex appearance-none">
+            <div className="w-full items-center hidden lg:flex lg:w-1/2 bg-cover rounded-l-lg">
+              <img src={backGroundImageBarberSignUp} alt="SignUp" />
+            </div>
+            <div className="w-full lg:w-1/2 rounded-lg lg:rounded-l-none">
+              <h3 className="text-Mauve-700 mt-12 text-2xl text-center">Sign Up Your Salon</h3>
+              <form className="mx-8 mt-6 pb-8 mb-4 rounded">
               <div className="mb-4">
-								<label className="block mb-2 text-sm font-bold text-gray-700" for="Email">
-									Email
-								</label>
-								<input
-									className="focus:placeholder-gray-500 focus:border-gray-600 w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-									id="Email"
-									type="email"
-									placeholder="Email"
-                  onChange={handleEmail}
-								/>
-                <p className="text-xs italic text-red-500">{emailAddressError}</p>
-							</div>
-              <div className="mb-4 md:flex md:justify-between">
-								<div className="relative mb-4 md:mr-2 md:mb-0">
-									<label className="block mb-2 text-sm font-bold text-gray-700" for="Password">
-										Password
-									</label>
-									<input
-										className="focus:placeholder-gray-500 focus:border-gray-600 w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-										id="Password"
-										type={isPasswordVisible ? "text" : "password"}
-										placeholder="******************"
-                    onChange={handlePassword}
-									/>
+                  <label className="block mb-2 text-sm font-bold text-Mauve-700" for="Username">
+                    Username
+                  </label>
+                  <div className="relative">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <svg className="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+                    </div>
+                    <input
+                      className="bg-charcoal-500 border-2 border-gray-400 focus:border-Mauve-700 w-full pl-10 py-2 text-base text-Mauve-700 leading-tight   rounded appearance-none focus:outline-none"
+                      id="Username"
+                      type="text"
+                      onChange={handleUsername}
+                    />
+                    <p className="m-1 text-xs italic text-red-500">{usernameError}</p>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <label className="text-Mauve-700 block mb-2 text-sm font-bold" for="Email">
+                    Email
+                  </label>
+                  <div className="relative">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <svg aria-hidden="true" class="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+                    </div>
+                    <input
+                      className="bg-charcoal-500 border-2 border-gray-400 focus:border-Mauve-700 w-full pl-10 py-2 text-base text-Mauve-700 leading-tight   rounded appearance-none focus:outline-none"
+                      id="Email"
+                      type="email"
+                      onChange={handleEmail}
+                    />
+                    <p className="text-xs italic text-red-500">{emailAddressError}</p>
+                  </div>
+                </div>
+                <div className="mb-4 md:flex md:justify-between">
+                  <div className="relative mb-4 md:mr-2 md:mb-0">
+                    <label className="text-Mauve-700 block mb-2 text-sm font-bold" for="Password">
+                      Password
+                    </label>
+                    <div className="relative">
+                      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path></svg>
+                      </div>
+                      <input
+                        className="border-gray-400 bg-charcoal-500 border-2 focus:border-Mauve-700 w-full pl-10 py-2 text-base text-Mauve-700 leading-tight   rounded appearance-none focus:outline-none"
+                        id="Password"
+                        type={isPasswordVisible ? "text" : "password"}
+                        onChange={handlePassword}
+                      />
+                      <p className="text-xs italic text-red-500"><p>{passwordError}</p></p>
+                    </div>
+                    <button
+                      type="button"
+                      className="absolute inset-y-12 right-0 flex items-center px-2 text-gray-400"
+                      onClick={togglePasswordVisibility}
+                    >
+                      {isPasswordVisible ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-5 h-5"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                          />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-5 h-5"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                          />
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                          />
+                        </svg>
+                      )}
+                    </button>
+                  </div>
+                  <div className="relative md:ml-2">
+                    <label className="text-Mauve-700 block mb-2 text-sm font-bold" for="C_password">
+                      Confirm Password
+                    </label>
+                    <div className="relative">
+                      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path></svg>
+                      </div>
+                      <input
+                        className="border-gray-400 bg-charcoal-500 border-2 focus:border-Mauve-700 w-full pl-10 py-2 text-base text-Mauve-700 leading-tight   rounded appearance-none focus:outline-none"
+                        id="C_password"
+                        type={isCPasswordVisible ? "text" : "password"}
+                        onChange={handleConfirmPassword}
+                      />
+                      <p className="text-xs italic text-red-500"><p>{confirmPasswordError}</p></p>
+                    </div>
+                    <button
+                      type="button"
+                      className="absolute inset-y-12 right-0 flex items-center px-2 text-gray-400"
+                      onClick={toggleCPasswordVisibility}
+                    >
+                      {isCPasswordVisible ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-5 h-5"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                          />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-5 h-5"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                          />
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                          />
+                        </svg>
+                      )}
+                    </button>
+                  </div>
+                </div>
+                <div className="mb-4 text-center">
                   <button
+                    className="w-full py-2 font-bold text-white bg-Mauve-700 rounded focus:outline-none focus:shadow-outline"
                     type="button"
-                    className="absolute inset-y-11 right-0 flex items-center px-4 text-gray-600"
-                    onClick={togglePasswordVisibility}
+                    onClick={handleSubmit}
                   >
-                    {isPasswordVisible ? (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-5 h-5"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                        />
-                      </svg>
-                    ) : (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-5 h-5"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
-                      </svg>
-                    )}
+                    Register Your Salon
                   </button>
-									<p className="text-xs italic text-red-500"><p>{passwordError}</p></p>
-								</div>
-								<div className="relative md:ml-2">
-									<label className="block mb-2 text-sm font-bold text-gray-700" for="C_password">
-										Confirm Password
-									</label>
-									<input
-										className="focus:placeholder-gray-500 focus:border-gray-600 w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-										id="C_password"
-										type={isCPasswordVisible ? "text" : "password"}
-										placeholder="******************"
-                    onChange={handleConfirmPassword}
-									/>
-                  <button
-                    type="button"
-                    className="absolute inset-y-11 right-0 flex items-center px-4 text-gray-600"
-                    onClick={toggleCPasswordVisibility}
+                  <p className="m-1 text-xs italic text-red-500">{submitError}</p>
+                </div>
+                <hr className="mb-3 border-t text-gray-400" />
+                <div className="py-2 w-full rounded bg-gray-600 text-center">
+                  <Link
+                    to="/LoginBarber"
+                    className="inline-block text-sm text-white align-baseline"
                   >
-                    {isCPasswordVisible ? (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-5 h-5"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                        />
-                      </svg>
-                    ) : (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-5 h-5"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
-                      </svg>
-                    )}
-                  </button>
-                  <p className="text-xs italic text-red-500"><p>{confirmPasswordError}</p></p>
-								</div>
-							</div>
-              <div className="mb-6 text-center">
-								<button
-									className="w-full px-4 py-2 font-bold text-white bg-purple-700 rounded-full hover:bg-purple-900 focus:outline-none focus:shadow-outline"
-									type="button"
-                  onClick={handleSubmit}
-								>
-									Register Your Salon
-								</button>
-                <p>{submitError}</p>
-							</div>
-              <hr className="mb-6 border-t" />
-              <div className="text-center">
-								<Link
-                  to="/LoginBarber"
-									className="inline-block text-sm text-purple-700 align-baseline hover:text-purple-900"
-								>
-									Already have an account? Login!
-								</Link>
-							</div>
-            </form>
+                    Already have an account? Login!
+                  </Link>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
