919e44b804f22fa015082f02fcd449743b0fa9a0
Ehsan <ehsan.ahmadpoor2002@gmail.com>
2023-05-14 13:13:23 +0330

    minor changes

--- frontend/src/pages/LoginSignup/LoginBarber.jsx
+++ frontend/src/pages/LoginSignup/LoginBarber.jsx
@@ -57,10 +57,11 @@ function LoginBarber(){
   }
 
   return(
-    <div className="bg-charcoal-500 flex items-center h-screen">
+    <div className="bg-backGround-500 min-h-screen">
+    <div className=" flex items-center">
       <div className="container mx-auto">
         <div className="flex justify-center mx-3">
-          <div className="bg-charcoal-500 w-full flex appearance-none">
+          <div className="bg-backGround-500 w-full flex appearance-none">
             <div className="w-full lg:flex items-center hidden lg:w-1/2 bg-cover rounded-l-lg">
               <img src={backgroundImageLoginBarber} alt="Login"/>
             </div>
@@ -76,7 +77,7 @@ function LoginBarber(){
                       <svg aria-hidden="true" class="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
                     </div>
                     <input
-                      className="bg-charcoal-500 border-2 border-gray-400 focus:border-Mauve-700 w-full pl-10 py-2 text-base text-Mauve-700 leading-tight   rounded appearance-none focus:outline-none"
+                      className="bg-backGround-500 border-2 border-gray-400 focus:border-Mauve-700 w-full pl-10 py-2 text-base text-Mauve-700 leading-tight   rounded appearance-none focus:outline-none"
                       id="Email"
                       type="email"
                       onChange={handleEmail}
@@ -93,7 +94,7 @@ function LoginBarber(){
                       <svg aria-hidden="true" class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path></svg>
                     </div>
                     <input
-                      className="border-gray-400 bg-charcoal-500 border-2 focus:border-Mauve-700 w-full pl-10 py-2 text-base text-Mauve-700 leading-tight   rounded appearance-none focus:outline-none"
+                      className="border-gray-400 bg-backGround-500 border-2 focus:border-Mauve-700 w-full pl-10 py-2 text-base text-Mauve-700 leading-tight   rounded appearance-none focus:outline-none"
                       id="Password"
                       type={isPasswordVisible ? "text" : "password"}
                       onChange={handlePassword}
@@ -164,6 +165,7 @@ function LoginBarber(){
           </div>
         </div>
       </div>
+    </div>
     </div>
   )
 }
--- frontend/src/pages/LoginSignup/LoginCustomer.jsx
+++ frontend/src/pages/LoginSignup/LoginCustomer.jsx
@@ -55,12 +55,13 @@ function LoginCustomer(){
     setpassword(event.target.value);
   }
   return(
-    <div className="bg-charcoal-500 flex items-center h-screen">
+    <div className="bg-backGround-500 min-h-screen">
+    <div className="flex items-center">
       <div className="container mx-auto">
         <div className="flex justify-center mx-3">
-          <div className="bg-charcoal-500 w-full flex appearance-none">
+          <div className="bg-backGround-500 w-full flex appearance-none">
             <div className="w-full hidden lg:flex items-center lg:w-1/2 bg-cover rounded-l-lg">
-              <img  src={backgroundImageLoginCustomer} alt="Login"/>
+              <img src={backgroundImageLoginCustomer} alt="Login"/>
             </div>
             <div className="w-full lg:w-1/2 p-5 rounded-lg lg:rounded-l-none">
               <h3 className="text-Mauve-700 mt-12 text-2xl text-center">Login</h3>
@@ -74,7 +75,7 @@ function LoginCustomer(){
                       <svg aria-hidden="true" class="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
                     </div>
                     <input
-                      className="bg-charcoal-500 border-2 border-gray-400 focus:border-Mauve-700 w-full pl-10 py-2 text-base leading-tight text-Mauve-700 rounded appearance-none focus:outline-none"
+                      className="bg-backGround-500 border-2 border-gray-400 focus:border-Mauve-700 w-full pl-10 py-2 text-base leading-tight text-Mauve-700 rounded appearance-none focus:outline-none"
                       id="Email"
                       type="email"
                       onChange={handleEmail}
@@ -91,7 +92,7 @@ function LoginCustomer(){
                       <svg aria-hidden="true" class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path></svg>
                     </div>
                     <input
-                      className="border-gray-400 bg-charcoal-500 border-2 focus:border-Mauve-700 w-full pl-10 py-2 text-base text-Mauve-700 leading-tight   rounded appearance-none focus:outline-none"
+                      className="border-gray-400 bg-backGround-500 border-2 focus:border-Mauve-700 w-full pl-10 py-2 text-base text-Mauve-700 leading-tight   rounded appearance-none focus:outline-none"
                       id="Password"
                       type={isPasswordVisible ? "text" : "password"}
                       onChange={handlePassword}
@@ -143,7 +144,7 @@ function LoginCustomer(){
                 </div>
                 <div className="mb-4 text-center">
                   <button
-                    className="w-full py-2 font-bold text-white bg-Mauve-700 rounded focus:outline-none focus:shadow-outline"
+                    className="w-full py-2 font-bold text-backGround-500 bg-Mauve-700 rounded focus:outline-none focus:shadow-outline"
                     type="button"
                     onClick={handleLogin}
                   >
@@ -162,6 +163,7 @@ function LoginCustomer(){
           </div>
         </div>
       </div>
+    </div>
     </div>
   )
 }
