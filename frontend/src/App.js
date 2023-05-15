a28f534529d7263d94b8cf86cd3d5747e24229cf
Ehsan <ehsan.ahmadpoor2002@gmail.com>
2023-05-15 21:39:24 +0330

    merge app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,10 @@ import SalonSelect from "./pages/SalonSelect/salonSelect";
 import SideBar from "./Components/SideBar/SideBar";
 import ImageSlider from "./pages/SalonPage/ImageSlider";
 import { SliderData }  from "./Components/SliderData";
+import ProfilePage from "./pages/customerProfile/Customer_Profile";
+import CashWithdrawal from "./pages/Wallet/Wallet";
+import { Receipt } from "./pages/Wallet/Receipt";
+import PaymentMethods from "./pages/ShoppingCart/shoppingcart";
 
 function App() {
   return (
@@ -30,12 +34,30 @@ function App() {
             <ImageSlider slides={SliderData}/>
           </div>
         } />
-        {/* <Route path="Profile" element ={
+        <Route path="/ProfilePage" element ={
           <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
             <SideBar/>
             <ProfilePage/>
           </div>
-        }/> */}
+        } />
+        <Route path="/wallet" element ={
+          <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
+            <SideBar/>
+            <CashWithdrawal/>
+          </div>
+        } />
+        <Route path="/payment" element ={
+          <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
+            <SideBar/>
+            <Receipt/>
+          </div>
+        } />
+        <Route path="/ShoppingCart" element ={
+          <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
+            <SideBar/>
+            <PaymentMethods/>
+          </div>
+        } />
       </Routes>
     </Router>
     </div>
