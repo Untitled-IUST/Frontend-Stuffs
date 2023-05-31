88a31b2197d39211bb341ec415a4ec1a60fb3e6e
Amir Deldar <ffereydounifargol@gmail.com>
2023-05-31 10:31:36 +0330

    fixed style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,11 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-}from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LoginCustomer from "./pages/LoginSignup/LoginCustomer";
 import SignUpCustomer from "./pages/LoginSignup/SignUpCustomer";
 import SalonSelect from "./pages/SalonSelect/salonSelect";
 import SideBar from "./Components/SideBar/SideBar";
 import ImageSlider from "./pages/SalonPage/ImageSlider";
-import { SliderData }  from "./Components/SliderData";
+import { SliderData } from "./Components/SliderData";
 import ProfilePage from "./pages/customerProfile/Customer_Profile";
 import CashWithdrawal from "./pages/Wallet/Wallet";
 import { Receipt } from "./pages/Wallet/Receipt";
@@ -22,58 +18,60 @@ import CommentExampleReplyFormOuter from "./pages/SalonPage/Comments";
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Routes>
-        <Route path="/" Component={Landing}/>
-        <Route path="/LoginCustomer" Component={LoginCustomer} />
-        <Route path="/SignUpCustomer" Component={SignUpCustomer} />
-        <Route path="/SalonSelect" element={
-          <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
-            <SideBar/>
-            <SalonSelect/>
-          </div>
-        }/>
-        <Route path="/SalonPage/:id" element ={
-          <ImageSlider slides={SliderData}/>
-        } />
-        {/* <Route path="/ProfilePage" element ={
+      <Router>
+        <Routes>
+          <Route path="/" Component={Landing} />
+          <Route path="/LoginCustomer" Component={LoginCustomer} />
+          <Route path="/SignUpCustomer" Component={SignUpCustomer} />
+          <Route
+            path="/SalonSelect"
+            element={
+              <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
+                <SideBar />
+                <SalonSelect />
+              </div>
+            }
+          />
+          <Route
+            path="/SalonPage/:id"
+            element={<ImageSlider slides={SliderData} />}
+          />
+          {/* <Route path="/ProfilePage" element ={
           <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
             <SideBar/>
             <ProfilePage/>
           </div>
         } /> */}
-        <Route path="/ProfilePage" Component={ProfilePage} />
-        <Route path="/wallet" Component={CashWithdrawal} />
+          <Route path="/ProfilePage" Component={ProfilePage} />
+          <Route path="/wallet" Component={CashWithdrawal} />
 
-        {/* <Route path="/wallet" element ={
+          {/* <Route path="/wallet" element ={
           <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
             <SideBar/>
             <CashWithdrawal/>
           </div>
         } /> */}
-        {/* <Route path="/payment" element ={
+          {/* <Route path="/payment" element ={
           <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
             <SideBar/>
             <Receipt/>
           </div>
         } /> */}
-        <Route path="/payment" Component={Receipt} />
-        <Route path="/ShoppingCart" element ={
-          <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
-            <SideBar/>
-            <PaymentMethods/>
-          </div>
-        } />
-        <Route path="/paymentcard"  Component={SelectedPlan} />
-      </Routes>
-    </Router>
+          <Route path="/payment" Component={Receipt} />
+          <Route
+            path="/ShoppingCart"
+            element={
+              <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
+                <SideBar />
+                <PaymentMethods />
+              </div>
+            }
+          />
+          <Route path="/paymentcard" Component={SelectedPlan} />
+        </Routes>
+      </Router>
     </div>
-    
   );
-  }
-
-
-
-
+}
 
 export default App;
--- frontend/src/pages/Landing/Landing.jsx
+++ frontend/src/pages/Landing/Landing.jsx
@@ -26,45 +26,7 @@ import Container from "@mui/material/Container";
 export const Landing = () => {
   const navigate = useNavigate();
   const [landingState, setLandingState] = useState("");
-  const tiers = [
-    {
-      title: "1 MONTH",
-      price: "5",
-      description: [
-        "10 users included",
-        "2 GB of storage",
-        "Help center access",
-        "Email support",
-      ],
-      buttonText: "BUY",
-      buttonVariant: "contained",
-    },
-    {
-      title: "3 MONTHS",
-      subheader: "Most popular",
-      price: "15",
-      description: [
-        "20 users included",
-        "10 GB of storage",
-        "Help center access",
-        "Priority email support",
-      ],
-      buttonText: "BUY",
-      buttonVariant: "contained",
-    },
-    {
-      title: "6 MONTHS",
-      price: "25",
-      description: [
-        "50 users included",
-        "30 GB of storage",
-        "Help center access",
-        "Phone & email support",
-      ],
-      buttonText: "BUY",
-      buttonVariant: "contained",
-    },
-  ];
+  
   // TODO remove, this demo shouldn't need to reset the theme.
   const defaultTheme = createTheme();
 
@@ -82,105 +44,24 @@ export const Landing = () => {
         <button className="nameButton" onClick={() => navigate("/")}>
           UNTITLED
         </button>
-        <button className="navButtons" onClick={() => navigate("/user/home")}>
+        <button className="navButtons" onClick={() => navigate("/SalonSelect")}>
           Hairdressers
         </button>
-        <button className="navLogin" onClick={() => navigate("/login")}>
+        <button className="navLogin" onClick={() => navigate("/LoginCustomer")}>
           Login/Register
         </button>
       </div>
       <div className="landingTitleHolder">
-        <p className="landingTitle">Fargol Beauty</p>
+        <p className="landingTitle">Untitled Beauty</p>
+        <p className="landingQuotes">Everything has beauty, but not everyone sees it.</p>
         <br></br>
         <br></br>
         <br></br>
+        <br></br>
+        
+        
 
-        <ThemeProvider theme={defaultTheme}>
-          <GlobalStyles
-            styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
-          />
-          <CssBaseline />
-
-          <Container maxWidth="md" component="main" className="middleContainer">
-            <Grid container spacing={5} alignItems="flex-end">
-              {tiers.map((tier) => (
-                // Enterprise card is full width at sm breakpoint
-                <Grid
-                  item
-                  key={tier.title}
-                  xs={12}
-                  sm={tier.title === "Enterprise" ? 12 : 6}
-                  md={4}
-                >
-                  <Card>
-                    <CardHeader
-                      title={tier.title}
-                      subheader={tier.subheader}
-                      titleTypographyProps={{ align: "center" }}
-                      action={tier.title === "Pro" ? <StarIcon /> : null}
-                      subheaderTypographyProps={{
-                        align: "center",
-                      }}
-                      sx={{
-                        backgroundColor: (theme) =>
-                          theme.palette.mode === "light"
-                            ? theme.palette.grey[200]
-                            : theme.palette.grey[700],
-                      }}
-                    />
-                    <CardContent>
-                      <Box
-                        sx={{
-                          display: "flex",
-                          justifyContent: "center",
-                          alignItems: "baseline",
-                          mb: 2,
-                        }}
-                      >
-                        <Typography
-                          component="h2"
-                          variant="h3"
-                          color="text.primary"
-                        >
-                          ${tier.price}
-                        </Typography>
-                      </Box>
-                      <ul>
-                        {tier.description.map((line) => (
-                          <Typography
-                            component="li"
-                            variant="subtitle1"
-                            align="center"
-                            key={line}
-                          >
-                            {line}
-                          </Typography>
-                        ))}
-                      </ul>
-                    </CardContent>
-                    <CardActions>
-                      <Button
-                        fullWidth
-                        variant={tier.buttonVariant}
-                        sx={{
-                          backgroundColor: "#ac3b61",
-                          "&:hover": {
-                            backgroundColor: "#eee2dc",
-                            color: "#000000",
-                          },
-                        }}
-                      >
-                        {tier.buttonText}
-                      </Button>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
-          </Container>
-        </ThemeProvider>
-
-        <button className="landingButton" onClick={() => navigate("/login")}>
+        <button className="landingButton" onClick={() => navigate("/LoginCustomer")}>
           LOGIN NOW !
         </button>
       </div>
